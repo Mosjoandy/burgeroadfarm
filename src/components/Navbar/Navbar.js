163f235610b1f1a15a54b2d7a9a506082d9a9f0e
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-09-21 17:28:26 -0700

    about page, need pics, decide on font and vet pic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Container } from "./../grid/"
+import { Container, Col } from "./../grid/"
 import "./Navbar.css"
 
 class Navbar extends Component {
@@ -8,32 +8,42 @@ class Navbar extends Component {
         return (
             <Container>
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <img id="burgeLogo" src={require("../../images/burge.png")} alt="brfLogo" />
+                    <Col size="md-3">
+                        <img id="burgeLogo" src={require("../../images/burge.png")} alt="brfLogo" />
+                    </Col>
+                    <Col size="md-9">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <Link to="/" id="navButtons" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                            <li><Link to="/" id="navButtons" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
                                 Home
-                            </Link> 
-                            <Link to="/Events" id="navButtons" className={window.location.pathname === "/events" ? "nav-link active" : "nav-link"}>
+                            </Link>
+                            </li>
+                            <li><Link to="/Events" id="navButtons" className={window.location.pathname === "/events" ? "nav-link active" : "nav-link"}>
                                 Events
                             </Link>
-                            <Link to="/About" id="navButtons" className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                            </li>
+                            <li><Link to="/About" id="navButtons" className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
                                 About
                             </Link>
-                            <Link to="/Contact" id="navButtons" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                            </li>
+                            <li><Link to="/Contact" id="navButtons" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
                                 Contact
                             </Link>
-                            <Link to="/Directions" id="navButtons" className={window.location.pathname === "/directions" ? "nav-link active" : "nav-link"}>
+                            </li>
+                            <li><Link to="/Directions" id="navButtons" className={window.location.pathname === "/directions" ? "nav-link active" : "nav-link"}>
                                 Directions
                             </Link>
-                            <Link to="/Media" id="navButtonsLast" className={window.location.pathname === "/media" ? "nav-link active" : "nav-link"}>
+                            </li>
+                            <li><Link to="/Media" id="navButtonsLast" className={window.location.pathname === "/media" ? "nav-link active" : "nav-link"}>
                                 Media
                             </Link>
+                            </li>
                         </div>
                     </div>
+                    </Col>
                 </nav>
             </Container>
         );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Container, Col, Row } from "../components/grid"
+import "./About.css";
+import { Container, Col, Row } from "../components/grid";
 
 const About = () => (
 
     <Container>
         <Row>
-            <Col size="xl-12">
-                About garbage
+            <Col size="xl-7">
+                <div id="aboutTitle">
+                    About Us
+                </div>
+                <div id="aboutBody">
+                    <p>We are a small, family owned and operated farm located  in Stockton, California.</p>
+                    <br />
+                    <p>Our mission is to provide organically fruits and vegetables for everyone to enjoy.
+                    We want to help people get back in touch with the food they eat and have a fun time
+                    at the farm!</p>
+                </div>
+                <div >
+                    <img id="vetOwned" src={require("../images/vetowned.png")} alt="vet" />
+                </div>
+            </Col>
+            <Col size="xl-5">
+                <div id="aboutPics">
+                    <div>
+                        <img id="aboutPic1" src="https://via.placeholder.com/400x250" alt="about1" />
+                    </div>
+                    <br />
+                    <div>
+                        <img id="aboutPic2" src="https://via.placeholder.com/400x250" alt="about2" />
+                    </div>
+                </div>
             </Col>
         </Row>
     </Container>
