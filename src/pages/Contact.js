5bbd0807b0b51dda0d63a534cb612436266b7006
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-09-22 17:42:25 -0700

    directions page added, google map embed added, adjust contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,89 @@
 import React from 'react';
-import { Container, Col, Row } from "../components/grid"
+import "./Contact.css";
+import { Container, Col, Row } from "../components/grid";
 
-const Contact = () => (
+class Contact extends React.Component {
 
-    <Container>
-        <Row>
-            <Col size="xl-12">
-                Contact garbage
-            </Col>
-        </Row>
-    </Container>
-);
+    constructor() {
+        super()
+        this.state = {
+            email: "",
+            name: "",
+            // comments: "",
+            // mailer: "false"
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("Name: " + this.state.name);
+        console.log("Email: " + this.state.email);
+        // console.log("Comment: " + this.state.comments);
+        // console.log("Mailer: " + this.state.mailer);
+        event.target.reset();
+    };
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-6">
+                        <div id="contactTitle">
+                            Oscar Richard Grado
+                        </div>
+                        <div id="contactBody">
+                            <p>Owner/Operator</p>
+                            <div><a href="tel:+12098171650" target="_blank" rel="noopener noreferrer">
+                                <img id="contactIco" src={require("../images/telephone.png")} alt="tele" />
+                                (209) 817-1650
+                            </a></div>
+                            <div><a href="mailto:burgeroadfarm@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <img id="contactIco" src={require("../images/email.png")} alt="mail" />
+                                burgeroadfarm@gmail.com
+                            </a></div>
+                            <div><a href="https://goo.gl/maps/FVcHZag9vZJ2" target="_blank" rel="noopener noreferrer">
+                                <img id="contactIco" src={require("../images/map.png")} alt="gmap" />
+                                3000 Burge Rd, Stockton, CA 95215
+                            </a></div>
+                        </div>
+                    </Col>
+                    <Col size="md-6">
+                        <div id="contactTitle">
+                            Contact
+                        </div>
+                        <div id="contactBody">
+                            <p>Sign up to be part of mailing list and be notified of upcoming events at
+                                the farm like cherry season opening.
+                            </p>
+                        </div>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label className="contactLabel">Name: </label>
+                                <input type="text" className="form-control mt-1 mb-3" name="name" onChange={this.handleChange} required />
+
+                                <label className="contactLabel">Email: </label>
+                                <input type="email" className="form-control mt-1 mb-3" name="email" onChange={this.handleChange} required />
+
+                                {/* <label className="contactLabel">Comments: </label> 
+                                <textarea type="text" className="form-control mt-1 mb-3" rows="3" name="comments" onChange={this.handleChange} />
+
+
+                                <label className="form-check-label contactLabel">Recieve Email Updates:</label>
+                                <input type="checkbox" className="ml-1 mt-3" name="mailer" />*/}
+                            </div>
+                            <button type="submit" className="btn contactLabel submitBtn" value="Submit">Sign Up</button>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    };
+};
 
 export default Contact
--- src/pages/Directions.js
+++ src/pages/Directions.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import "./Directions.css"
 import { Container, Col, Row } from "../components/grid"
 
 const Directions = () => (
 
     <Container>
         <Row>
-            <Col size="xl-12">
-                Directions garbage
+            <Col size="xl-7">
+                <div id="directionsTitle">
+                    Directions
+                </div>
+                <div id="directionsBody">
+                    <ol type="1">
+                        <li className="listDirect">From Sacramento, take CA-99 South to Stockton</li>
+                        <li className="listDirect">Take the Waterloo Rd/HWY-88 exit</li>
+                        <li className="listDirect">Head East towards Jackson</li>
+                        <li className="listDirect">Turn right on to Fairchild Rd</li>
+                        <li className="listDirect">Turn right on to Burge Rd</li>
+                        <li className="listDirect">Go to the very end of Burge Rd</li>
+                        <li className="listDirect">Pass the railroad tracks</li>
+                        <li className="listDirect">Follow the road as it curves left</li>
+                        <li className="listDirect">Keep going until you see the sign</li>
+                        <li className="listDirect">Call us if you get lost :)</li>
+                    </ol>
+                    <div className="ml-3">
+                        <a href="tel:+12098171650" target="_blank" rel="noopener noreferrer">
+                            <img id="contactIco" src={require("../images/telephone.png")} alt="tele" />
+                            (209) 817-1650
+                        </a>
+                    </div>
+                </div>
+            </Col>
+            <Col size="xl-5">
+                <div id="directionsBody">
+                    <div className="mapouter card">
+                        <div className="gmap_canvas">
+                            <iframe title="gmapBRF" width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=burge%20road%20farm&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0">
+                            </iframe>
+                        </div>
+                    </div>
+                    <div className="card-footer bg-white">
+                        <a className="text-center " href="https://goo.gl/maps/FVcHZag9vZJ2" target="_blank" rel="noopener noreferrer">
+                            <img id="contactIco" src={require("../images/map.png")} alt="gmap" />
+                            3000 Burge Rd, Stockton, CA 95215
+                        </a>
+                    </div>
+                </div>
             </Col>
         </Row>
     </Container>
