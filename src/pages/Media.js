28fd1d6d3d775043a9af62e849332bb8acef7260
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-09-28 15:58:04 -0700

    media update

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,51 +1,76 @@
 import React from 'react';
 import "./Media.css";
 import { Container, Col, Row } from "../components/grid";
+import Fade from "react-reveal/Fade";
 
 const Media = () => (
 
     <Container>
         <Row>
             <Col size="xl-12">
-                <div className="mediaBody">
-                    <div id="carousel" className="carousel slide carousel-fade" data-ride="carousel">
-                        <ol className="carousel-indicators">
-                            <li data-target="#carousel" data-slide-to="0" className="active"></li>
-                            <li data-target="#carousel" data-slide-to="1"></li>
-                            <li data-target="#carousel" data-slide-to="2"></li>
-                        </ol>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img className="item d-block w-100" src="https://via.placeholder.com/800x300" alt="First slide" />
-                                <div class="carousel-caption">
-                                    <h4>Heading 1</h4>
-                                    <p>Details....</p>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <img className="item d-block w-100" src="https://via.placeholder.com/800x400" alt="Second slide" />
-                                <div class="carousel-caption">
-                                    <h4>Heading 2</h4>
-                                    <p>Details....</p>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <img className="item d-block w-100" src="https://via.placeholder.com/700x300" alt="Third slide" />
-                                <div class="carousel-caption">
-                                    <h4>Heading 3</h4>
-                                    <p>Details....</p>
-                                </div>
+                <div className="card-columns ">
+                    <Fade cascade timeout={2000}>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
                             </div>
                         </div>
-                        <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                    </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
+                            </div>
+                        </div>
+                        <div className="card border-0" >
+                            <div className="card-body">
+                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
+                            </div>
+                        </div>
+                    </Fade>
                 </div>
             </Col>
         </Row>
