ac9266c24fad244baa82855d2ce4a796d90c7296
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-10-02 02:23:23 -0700

    remove lightbox

---
+++ src/components/Lightbox.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+// class Lightbox extends React.Component {
+
+//     constructor() {
+//         super()
+//         this.state = {
+//             lightboxIsOpen: false,
+//         }
+//         this.gotoNext = this.gotoNext.bind(this);
+//         this.onClickPrev = this.gotoPrevious.bind(this);
+//         this.closeLightbox = this.closeLightbox.bind(this);
+//     }
+
+//   render() {
+//     return (
+//       <Lightbox
+//         images={[{ src: 'http://example.com/img1.jpg' }, { src: 'http://example.com/img2.jpg' }]}
+//         isOpen={this.state.lightboxIsOpen}
+//         onClickPrev={this.gotoPrevious}
+//         onClickNext={this.gotoNext}
+//         onClose={this.closeLightbox}
+//       />
+//     );
+//   }
+// }
+
+// export default Lightbox
--- src/pages/Media.js
+++ src/pages/Media.js
@@ -2,79 +2,94 @@ import React from 'react';
 import "./Media.css";
 import { Container, Col, Row } from "../components/grid";
 import Fade from "react-reveal/Fade";
+// import Lightbox from "react-images";
 
-const Media = () => (
+class Media extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+     
+        };
 
-    <Container>
-        <Row>
-            <Col size="xl-12">
-                <div className="card-columns ">
-                    <Fade cascade timeout={2000}>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
-                            </div>
-                        </div>
-                        <div className="card border-0" >
-                            <div className="card-body">
-                                <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
-                            </div>
-                        </div>
-                    </Fade>
-                </div>
-            </Col>
-        </Row>
-    </Container>
-);
+    
+    }     
+
+    render() {
+      
+        return (
+            <Container>
+                <Row>
+                    <Col size="xl-12">
+                      
+                        {/* <div className="card-columns ">
+                            <Fade cascade timeout={2000}>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/500x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/250x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/400x600" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/700x500" alt="about1" />
+                                    </div>
+                                </div>
+                                <div className="card border-0" >
+                                    <div className="card-body">
+                                        <img id="card-pic" className="rounded" src="https://via.placeholder.com/300x600" alt="about1" />
+                                    </div>
+                                </div>
+                            </Fade>
+                        </div> */}
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+};
 
 export default Media
