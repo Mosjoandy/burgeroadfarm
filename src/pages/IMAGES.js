0acd81a1e3b24a757f798fadfb911ce9cb65ca24
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-10-04 12:27:25 -0700

    separated IMAGES js file, adjust media sizes

--- src/pages/IMAGES.js
+++ src/pages/IMAGES.js
@@ -1,6 +1,4 @@
-export default IMAGES
-
-const IMAGES =
+export const IMAGES =
 
     [
         {
@@ -8,7 +6,7 @@ const IMAGES =
             thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512345/BRF/2.jpg",
             thumbnailWidth: 240,
             thumbnailHeight: 180
-            
+
         },
         {
             src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512334/BRF/16.jpg",
@@ -21,6 +19,92 @@ const IMAGES =
             thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512364/BRF/13.jpg",
             thumbnailWidth: 432,
             thumbnailHeight: 545
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/9.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/9.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 180
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/6.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/6.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512353/BRF/18.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512353/BRF/18.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512352/BRF/8.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512352/BRF/8.jpg",
+            thumbnailWidth: 256,
+            thumbnailHeight: 192
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512346/BRF/3.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512346/BRF/3.jpg",
+            thumbnailWidth: 280,
+            thumbnailHeight: 576
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512345/BRF/4.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512345/BRF/4.jpg",
+            thumbnailWidth: 280,
+            thumbnailHeight: 576
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512366/BRF/12.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512366/BRF/12.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/11.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512361/BRF/11.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512360/BRF/5.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512360/BRF/5.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 504
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512360/BRF/10.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512360/BRF/10.jpg",
+            thumbnailWidth: 240,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512358/BRF/19.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512358/BRF/19.jpg",
+            thumbnailWidth: 256,
+            thumbnailHeight: 192
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512340/BRF/1.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512340/BRF/1.jpg",
+            thumbnailWidth: 216,
+            thumbnailHeight: 384
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512334/BRF/15.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512334/BRF/15.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 320
+        },
+        {
+            src: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512349/BRF/17.jpg",
+            thumbnail: "https://res.cloudinary.com/mosjoandy/image/upload/v1538512349/BRF/17.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 320
         }
-        
+
     ]
+
+export default IMAGES
