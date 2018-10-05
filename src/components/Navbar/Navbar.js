683fe816e818908b099dc3bfa7a3945bd318ba8b
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-10-04 23:38:34 -0700

    navbar icon goes home. emailer button update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,40 +9,42 @@ class Navbar extends Component {
             <Container>
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <Col size="md-3">
-                        <img id="burgeLogo" src={require("../../images/burge.png")} alt="brfLogo" />
+                        <Link to="/" className={window.location.pathname === "/"}>
+                            <img id="burgeLogo" src={require("../../images/burge.png")} alt="brfLogo" />
+                        </Link>
                     </Col>
                     <Col size="md-9">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <li><Link to="/" id="navButtons" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-                                Home
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <li><Link to="/" id="navButtons" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                                    Home
                             </Link>
-                            </li>
-                            <li><Link to="/Events" id="navButtons" className={window.location.pathname === "/events" ? "nav-link active" : "nav-link"}>
-                                Events
+                                </li>
+                                <li><Link to="/Events" id="navButtons" className={window.location.pathname === "/events" ? "nav-link active" : "nav-link"}>
+                                    Events
                             </Link>
-                            </li>
-                            <li><Link to="/About" id="navButtons" className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
-                                About
+                                </li>
+                                <li><Link to="/About" id="navButtons" className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                                    About
                             </Link>
-                            </li>
-                            <li><Link to="/Contact" id="navButtons" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
-                                Contact
+                                </li>
+                                <li><Link to="/Contact" id="navButtons" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                                    Contact
                             </Link>
-                            </li>
-                            <li><Link to="/Directions" id="navButtons" className={window.location.pathname === "/directions" ? "nav-link active" : "nav-link"}>
-                                Directions
+                                </li>
+                                <li><Link to="/Directions" id="navButtons" className={window.location.pathname === "/directions" ? "nav-link active" : "nav-link"}>
+                                    Directions
                             </Link>
-                            </li>
-                            <li><Link to="/Media" id="navButtonsLast" className={window.location.pathname === "/media" ? "nav-link active" : "nav-link"}>
-                                Media
+                                </li>
+                                <li><Link to="/Media" id="navButtonsLast" className={window.location.pathname === "/media" ? "nav-link active" : "nav-link"}>
+                                    Media
                             </Link>
-                            </li>
+                                </li>
+                            </div>
                         </div>
-                    </div>
                     </Col>
                 </nav>
             </Container>
--- src/pages/Emailer.js
+++ src/pages/Emailer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Col, Row } from "../components/grid";
 import "./Emailer.css"
 import firebase from "../firebase";
+import Fade from "react-reveal/Fade";
 
 class Emailer extends React.Component {
 
@@ -9,6 +10,7 @@ class Emailer extends React.Component {
         super()
         this.state = {
             mailerList: [],
+            copied: false,
         };
         this.copyToClipboard = this.copyToClipboard.bind(this)
     };
@@ -37,6 +39,7 @@ class Emailer extends React.Component {
     };
 
     copyToClipboard() {
+        this.setState({ copied: true });
         let theList = this.state.mailerList.map((index) =>
             " " + index.email)
         let textField = document.createElement('textarea')
@@ -52,36 +55,40 @@ class Emailer extends React.Component {
         return (
             <Container>
                 <Row>
-                    <Col size="md-5">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Remove</th>
-                                </tr>
-                                {this.state.mailerList.map((mailer) =>
-                                    <tr key={mailer.id}>
-                                        <td>{mailer.name}</td>
-                                        <td>{mailer.email}</td>
-                                        <td className="text-center"><button type="button" className="btn btn-danger" onClick={() => this.removeItem(mailer.id)}>X</button></td>
+                    <Col size="md-7">
+                        <Fade>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th className="text-center">Remove</th>
                                     </tr>
-                                )}
-                            </tbody>
-                        </table>
+                                    {this.state.mailerList.map((mailer) =>
+                                        <tr key={mailer.id}>
+                                            <Fade delay={1000} duration={2000}>
+                                                <td>{mailer.name}</td>
+                                                <td>{mailer.email}</td>
+                                                <td className="text-center"><button type="button" className="btn btn-danger" onClick={() => this.removeItem(mailer.id)}>X</button></td>
+                                            </Fade>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </Fade>
                     </Col>
                     <Col size="md-5">
-                        <button
-                            className="btn btn-success"
-                            type="button"
-                            onClick={this.copyToClipboard}
-                            data-toggle="popover"
-                            data-trigger="focus"
-                            title="Copy All"
-                            data-content="Copied to Clipboard"
-                        >
-                            Copy All
-                        </button>
+                        {!this.state.copied ?
+                            <Fade>
+                                <button className="btn btn-success" type="button" onClick={this.copyToClipboard}>
+                                    Copy All
+                                </button>
+                            </Fade>
+                            :
+                            <Fade>
+                                <img className="pl-2" id="smolCheck" src={require("../images/check.png")} alt="check mark" /><span>Copied to Clipboard</span>
+                            </Fade>
+                        }
                         <div className="card-body">
                             {this.state.mailerList.map((mailer) =>
                                 <span key={mailer.id}>{mailer.email}, </span>
