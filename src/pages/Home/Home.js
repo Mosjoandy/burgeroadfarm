ad7f92de4f6d8b26eb0ee22bfca1ec746db24cce
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2019-06-27 00:29:03 -0700

    closed pic update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => (
             <Slide bottom>
 
               <div className="card">
-                <img className="mx-auto" id="announcePic" src={require("../../images/finalfinalfinalform.jpg")} alt="cherryNow" />
+                <img className="mx-auto" id="announcePic" src={require("../../images/closingtime.jpg")} alt="cherryNow" />
 
                 {/* <img className="mx-auto" id="cherry" src={require("../../images/cherry.png")} alt="cherryTriple" /> */}
 
