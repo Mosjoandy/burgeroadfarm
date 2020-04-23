0e5096cf0a9ca2caedb355c433fdd2baf29bc711
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2020-04-22 23:13:46 -0700

    update home page, new pic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,25 +15,41 @@ const Home = () => (
             <Slide bottom>
 
               <div id="homeTitle">
-              
-                  We are closed for the season!
-        
+
+                <span role="img" aria-label="cherry">🍒</span>Cherry Season Coming Soon!<span role="img" aria-label="cherry">🍒</span>
+
               </div>
 
             </Slide>
             <Slide bottom>
 
               <div id="homeBody">
-                <p></p>
+
                 {/* <div id="homeTitle2">Open 8AM - 4PM</div> */}
                 {/* <p>We will stay open until cherries sell out</p> */}
-                <div id="homeTitle2">
-                Thank you everyone for supporting us!
-                <br />
-                <br />
-                 
-              </div>
-                <p>Please follow us on  {}
+
+                 <br />
+                <div id="homeTitle3">
+                  COVID-19 Update:
+                <div id="homeBody2">
+                    For everyone's safety and to practice social distancing, we will be open for cherry picking <b>BY APPOINTMENT ONLY</b> until further notice. Please stay tuned for more details and season opening dates.
+                </div>
+                  <br />
+                  <div id="homeTitle2">
+                    Please follow us on  <span><a rel="noopener noreferrer" href="https://www.facebook.com/burgeroadfarm/" target="_blank">
+                      <img id="homeIcon" src={require("../../images/facebook.png")} alt="facebook burge road farm" />
+                    </a></span>
+                    {} {}
+                    <span><a rel="noopener noreferrer" href="https://www.instagram.com/burgeroadfarm/" target="_blank">
+                      <img id="homeIcon" src={require("../../images/instagram.png")} alt="instagram burge road farm" />
+                    </a></span> for the latest updates.
+
+
+                  </div>
+
+
+                </div>
+                {/* <p>Please follow us on  {}
 
                   <span><a rel="noopener noreferrer" href="https://www.facebook.com/burgeroadfarm/" target="_blank">
                     <img id="homeIcon" src={require("../../images/facebook.png")} alt="facebook burge road farm" />
@@ -43,14 +59,16 @@ const Home = () => (
                     <img id="homeIcon" src={require("../../images/instagram.png")} alt="instagram burge road farm" />
                   </a></span>
 
-                  {}    for future events!</p>
+                  {}    for future events!
+                  
+                  </p> */}
               </div>
 
             </Slide>
             <Slide bottom>
 
               <div className="card">
-                <img className="mx-auto" id="announcePic" src={require("../../images/closingtime.jpg")} alt="cherryNow" />
+                <img className="mx-auto" id="announcePic" src={require("../../images/early_2020.jpg")} alt="cherryNow" />
 
                 {/* <img className="mx-auto" id="cherry" src={require("../../images/cherry.png")} alt="cherryTriple" /> */}
 
